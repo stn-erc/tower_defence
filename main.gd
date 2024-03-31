28bcf985b55aa363d0d6f5dec7b57181d76d7711
stn-erc <54769710+stn-erc@users.noreply.github.com>
2024-03-31 17:06:36 +0300

    Add files via upload

---
+++ main.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+
+func _on_quit_pressed():
+	get_tree().quit() # Replace with function body.
+
+#place holder
+func _ready():
+	get_tree().change_scene_to_file("res://world.tscn") # Replace with function body.
